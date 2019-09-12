5274bb02ae112443461ade375b248a4449301d01
Sidnei Quinelato <sidneiq@gmail.com>
2019-09-11 22:02:42 -0300

    usuario logado na tela inicial

--- src/services/domain/cliente.service.ts
+++ src/services/domain/cliente.service.ts
@@ -15,12 +15,14 @@ export class ClienteService {
 
     findByEmail(email: string) : Observable<ClienteDTO> {
 
-        let token = this.storage.getLocalUser().token;
+        /*let token = this.storage.getLocalUser().token;
         let authHeader = new HttpHeaders({'Authorization': 'Bearer ' + token});
 
         return this.http.get<ClienteDTO>(
             `${API_CONFIG.baseUrl}/clientes/email?value=${email}`,
-            {'headers': authHeader});
+            {'headers': authHeader});*/
+
+            return this.http.get<ClienteDTO>(`${API_CONFIG.baseUrl}/clientes/email?value=${email}`);   
     }
 
     getImageFromBucket(id : string) : Observable<any> {
